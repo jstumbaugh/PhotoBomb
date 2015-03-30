d63b534e8d8f4b97b9c14da156e6807bee005620
jstumbaugh <jstumbaugh@smu.edu>
2015-03-29 20:42:12 -0500

    trying to get the pictures to repeat in main.html using angular

--- website/js/main.js
+++ website/js/main.js
@@ -7,8 +7,41 @@ $( document ).ready( function() {
 
 });
 
-function pictureCtrl($scope) {
-	$scope.imgHTML = '<img src="https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg">';
-	$scope.picTitle = "Golfing Picture";
-	$scope.username = "johnny";
-}
+var app = angular.module('main',[]);
+app.controller('pictureCtrl', ['$scope', function($scope) {
+	$scope.imgsrc = "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg";
+	$scope.picTitle = "Golf Picture";
+	$scope.contributor = "johnny chimpo";
+
+	$scope.jsonArr = [
+		{
+			"pictureID": 1,
+			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
+			"picTitle": "Golf Picture",
+			"contributor": "johnny chimpo",
+			"currentVotes": 22
+		},
+		{
+			"pictureID": 1,
+			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
+			"picTitle": "Golf Picture",
+			"contributor": "johnny chimpo",
+			"currentVotes": 22
+		},
+		{
+			"pictureID": 1,
+			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
+			"picTitle": "Golf Picture",
+			"contributor": "johnny chimpo",
+			"currentVotes": 22
+		},
+		{
+			"pictureID": 1,
+			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
+			"picTitle": "Golf Picture",
+			"contributor": "johnny chimpo",
+			"currentVotes": 22
+		}
+	];
+
+}]);
