403ddde8ff5b40336145120ce2549e2acd677b81
jstumbaugh <jstumbaugh@smu.edu>
2015-03-30 12:26:48 -0500

    finished loading pics function, still need to set carousel size

--- website/js/main.js
+++ website/js/main.js
@@ -5,43 +5,36 @@ $( document ).ready( function() {
 		//alert("searched");
 	}); // end of search function
 
+	window.onload = loadPics();
+
 });
 
+// populate pictures
+function loadPics() {
+	// apiary API: http://private-5c6f-assignment51.apiary-mock.com/pics
+	$.get("http://private-5c6f-assignment51.apiary-mock.com/pics", function(data) {
+		var picWrapper1 = '<div class="col-sm-6 col-md-3"><div class="thumbnail"><img src="';
+		var picWrapper2 = '"><div class="caption"><h4>';
+		var picWrapper3 = '</h4><p>submitted by: ';
+		var picWrapper4 = '</p><p><a href="';
+		var picWrapper5 = '" class="btn btn-success" role="button">View Picture</a></p></div></div></div></div>';
+		var linkToPage = "#";
+
+		for (var i=0; i < data.length; i++) {
+			var finalStr = picWrapper1 + data[i].imgsrc + picWrapper2 + data[i].picTitle + picWrapper3 + data[i].contributor + picWrapper4 + linkToPage + picWrapper5;
+			document.getElementById("pictures").innerHTML += finalStr;
+		}
+	});
+}
+
+
+
+// For angular controller 
 var app = angular.module('main',[]);
 app.controller('pictureCtrl', ['$scope', function($scope) {
-	$scope.imgsrc = "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg";
-	$scope.picTitle = "Golf Picture";
-	$scope.contributor = "johnny chimpo";
+	// $scope.imgsrc = "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg";
+	// $scope.picTitle = "Golf Picture";
+	// $scope.contributor = "johnny chimpo";
 
-	$scope.jsonArr = [
-		{
-			"pictureID": 1,
-			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
-			"picTitle": "Golf Picture",
-			"contributor": "johnny chimpo",
-			"currentVotes": 22
-		},
-		{
-			"pictureID": 1,
-			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
-			"picTitle": "Golf Picture",
-			"contributor": "johnny chimpo",
-			"currentVotes": 22
-		},
-		{
-			"pictureID": 1,
-			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
-			"picTitle": "Golf Picture",
-			"contributor": "johnny chimpo",
-			"currentVotes": 22
-		},
-		{
-			"pictureID": 1,
-			"imgsrc": "https://www.palacasino.com/wp-content/uploads/2009/11/GolfImage.jpg",
-			"picTitle": "Golf Picture",
-			"contributor": "johnny chimpo",
-			"currentVotes": 22
-		}
-	];
-
+	
 }]);
