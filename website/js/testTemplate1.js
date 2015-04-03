1d81cbb5d4e020a4054ade0162e8400d848ddbbe
jstumbaugh <jstumbaugh@smu.edu>
2015-04-02 20:23:00 -0500

    added 2 more dummy pages so that i have 5 templates

---
+++ website/js/testTemplate1.js
@@ -0,0 +1,5 @@
+// For angular controller 
+var app1 = angular.module('testTemplate1',[]);
+app1.controller('test', function($scope) {
+	$scope.testData = "Hello, this is a dummy page. Here is some test data to fill it up.";
+});
---
+++ website/js/testTemplate2.js
@@ -0,0 +1,6 @@
+// For angular controller 
+var app1 = angular.module('testTemplate2',[]);
+app1.controller('test', function($scope) {
+	$scope.testData = "Hello, this is another dummy page that will show that I know how to use AngularJS";
+	$scope.jason = "My name is Jason Stumbaugh";
+});
