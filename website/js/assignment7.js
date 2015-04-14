dc37b32b535add7b51e114a85e5ab5baa28624d3
jstumbaugh <jstumbaugh@smu.edu>
2015-04-14 18:14:47 -0500

    finished A7

--- website/js/assignment7.js
+++ website/js/assignment7.js
@@ -45,11 +45,11 @@ function testGetFromParse(ID) {
 
 //testGetFromParse("RwIi5TXbL8");
 
-// ERROR IN THE CODE HERE SOMEWHERE
 function testGetCollectionFromParse() {
 	alert("Assignment 7: testGetCollectionFromParse function called. Look in console for output");
 	console.log("testGetCollectionFromParse output");
-
+	
+	var Picture = Parse.Object.extend("Picture");
 	var pictureCollection = Parse.Collection.extend({
 		model: Picture
 	});
@@ -59,7 +59,7 @@ function testGetCollectionFromParse() {
 		success: function(collection) {
 			console.log("Success retreiving collection of Picture");
 			collection.each(function(object) {
-				console.log(object);
+				console.warn(object.attributes);
 			});
 		},
 		error: function(collection, error) {
@@ -97,39 +97,33 @@ function testUpdateObjectParse(title, contributor, imgsrc, votes) {
 
 //testUpdateObjectParse("TEST", "stumbeast", "imgsrc", 1000);
 
-// THIS DOES NOT WORK AS WELL
-function testDeletObjectFromParse() {
+function testDeletObjectFromParse(ID) {
 	alert("Assignment 7: testDeletObjectFromParse function called. Look in console for output");
 	console.log("testDeletObjectFromParse output");
 
 	var Picture = Parse.Object.extend("Picture");
 	var pic = new Picture();
-	pic.set("picTitle", "title");
-	pic.set("contributor", "contributor");
-	pic.set("imgsrc", "imgsrc");
-	pic.set("votes", 0);
-	var ID = 0;
-	
-	// pic.save(null, {
-	// 	success: function(pic) {
-	// 		ID = pic.id;
-	// 	},
-	// 	error: function(pic, error) {
-	// 		console.log('Failed to create new object, with error code: ' + error.message);
-	// 	}
-	// });
+	var query = new Parse.Query(Picture);
 
-	pic.destroy({
-		success: function(picture) {
-			alert("success");
-			console.log("Newly created object was successfully deleted from Parse");
+	query.get(ID, {
+		success: function(pic) {
+			console.log("Success in retireving object, begin deleting it");
+			pic.destroy({
+				success: function(picture) {
+					alert("Success in deleting object from Parse");
+					console.log("Object was successfully deleted from Parse");
+				},
+				error: function(picture, error) {
+					console.log("Object was not successfully deleted from Parse. Error: " + error);
+				}
+			});
 		},
-		error: function(picture, error) {
-			console.log("Newly created object was not successfully deleted from Parse. Error: " + error);
+		error: function(object, error) {
+			console.log('Failed to retrieve object, with error code: ' + error.message);
 		}
 	});
 
 }
 
-testDeletObjectFromParse();
+//testDeletObjectFromParse("HoV34NDhig");
 
